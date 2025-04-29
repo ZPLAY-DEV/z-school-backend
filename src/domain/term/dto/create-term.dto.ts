16acddc65da291423dbbe20e89f31b7af7a1ddd0
Evelops <co6747@naver.com>
2025-04-29 14:05:34 +0900

    refactor: 운영기간관리 DTO swagger example logic 추가

--- src/domain/term/dto/create-term.dto.ts
+++ src/domain/term/dto/create-term.dto.ts
@@ -11,34 +11,44 @@ import {
 import { IsValidDateRange } from '../validator/date-range.validator';
 
 export class CreateTermDto {
-  @ApiProperty({ description: '🈳 DB의 학교ID' })
+  @ApiProperty({ description: '🈳 DB의 학교ID', example: 1 })
   @IsInt()
   @IsNotEmpty()
   @IsPositive()
   schoolId: number;
 
-  @ApiProperty({ description: '🈳 학교명', required: false })
+  @ApiProperty({
+    description: '🈳 학교명',
+    required: false,
+    example: '홍익대학교 사범대학 부속 초등학교',
+  })
   @IsOptional()
   @IsString()
   @MaxLength(16)
   schoolName?: string | null;
 
-  @ApiProperty({ description: '🈵 학사년도' })
+  @ApiProperty({ description: '🈵 학사년도', example: 2025 })
   @IsInt()
   @IsPositive()
   schoolYear: number;
 
-  @ApiProperty({ description: '🈵 학기명' })
+  @ApiProperty({ description: '🈵 학기명', example: '1학기' })
   @IsString()
   @MaxLength(16)
   termName: string;
 
-  @ApiProperty({ description: '🈵 ISO 형식의 날짜 문자열 (YYYY-MM-DD)' })
+  @ApiProperty({
+    description: '🈵 ISO 형식의 날짜 문자열 (YYYY-MM-DD)',
+    example: '2025-03-01',
+  })
   @IsString()
   @Length(10)
   start: string;
 
-  @ApiProperty({ description: '🈵 ISO 형식의 날짜 문자열 (YYYY-MM-DD)' })
+  @ApiProperty({
+    description: '🈵 ISO 형식의 날짜 문자열 (YYYY-MM-DD)',
+    example: '2025-09-04',
+  })
   @IsString()
   @Length(10)
   @IsValidDateRange('start', {
