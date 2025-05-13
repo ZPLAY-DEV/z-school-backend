7c48b95da2e08c62d4249c58cbb38ddb88f1bb36
Evelops <co6747@naver.com>
2025-05-13 19:02:52 +0900

    refactor: 학생 - 학생 조회시, 앱 사용유무 & groupStudent 값도 한번에 반환되도록 추가

--- src/domain/student/swagger/rest-swagger.decorator.ts
+++ src/domain/student/swagger/rest-swagger.decorator.ts
@@ -102,6 +102,8 @@ export const StudentListDocs = () => {
       - 학생 일괄 조회
       - 학생은 학년, 반, 학번/번호 순으로 정렬됨.
       - 페이징 X
+      - 반환되는 parent 객체에 userId 값이 null인 경우 앱 미사용 학부모, null이 아닌경우 앱 사용 유저라고 프론트에서 핸들링 
+      - 반환되는 groupStudents 객체 배열에 맞춰서 수강중인 강좌를 프론트에서 핸들링
       `,
     }),
     ApiParam({
@@ -168,6 +170,8 @@ export const StudentListPaginatedDocs = () => {
       - 정렬은 기본적으로 학교에 속한 학생들을 기준으로 grade ( 학년 ), class ( 반 ), studentCode ( 학번/번호 ) 순으로 정렬됨.
       - 정렬 조건: grade(학년), class(반), studentCode(학번/번호)
         - 정렬시 QueryString에 sortBy 키워드를 통해 정렬 조건을 입력할 수 있음. EX) ?sortBy=grade:ASC&sortBy=class:ASC&sortBy=studentCode:ASC ...
+      - 반환되는 parent 객체에 userId 값이 null인 경우 앱 미사용 학부모, null이 아닌경우 앱 사용 유저라고 프론트에서 핸들링 
+      - 반환되는 groupStudents 객체 배열에 맞춰서 수강중인 강좌를 프론트에서 핸들링
       `,
     }),
     ApiOkPaginatedResponse(StudentResponseDto, {
