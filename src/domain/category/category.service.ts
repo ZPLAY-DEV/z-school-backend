ca555274c3c06205a3cd719fd33f7a31eb406e16
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-06-20 20:45:59 +0900

    add frequency to the offering

--- src/domain/category/category.service.ts
+++ src/domain/category/category.service.ts
@@ -58,18 +58,22 @@ export class CategoryService {
       {
         slug: CategoryEnum.FREE_CUSTOM,
         name: CategoryLabels[CategoryEnum.FREE_CUSTOM],
+        note: '맞춤형 1,2학년 대상 (누구나 신청만 하면 듣는 수업)',
       },
       {
         slug: CategoryEnum.FREE_CARE,
         name: CategoryLabels[CategoryEnum.FREE_CARE],
+        note: '돌봄 (선착순, 추첨, 재수강우선 3방식 중 학교가 선택)',
       },
       {
         slug: CategoryEnum.FREE_OPTIONAL,
         name: CategoryLabels[CategoryEnum.FREE_OPTIONAL],
+        note: '방과후 (선착순, 추첨, 재수강우선 3방식 중 학교가 선택)',
       },
       {
         slug: CategoryEnum.PAID_OPTIONAL,
         name: CategoryLabels[CategoryEnum.PAID_OPTIONAL],
+        note: '방과후 (선착순, 추첨, 재수강우선 3방식 중 학교가 선택)',
       },
     ];
 
--- src/domain/category/entities/category.entity.ts
+++ src/domain/category/entities/category.entity.ts
@@ -26,6 +26,10 @@ export class Category {
   @Column({ length: 32 })
   name: string;
 
+  @ApiProperty({ description: '🈳 비고' })
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  note: string | null;
+
   @ApiProperty({ description: 'total count' })
   @Column({ type: 'int', unsigned: true, default: 0 })
   count: number;
