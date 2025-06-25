7dca74723d167d4b8128c7706d07bdaff2749601
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-06-25 17:52:21 +0900

    remove ngrok from ecosystem.config to be able to run independently w/ launchctl

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,16 +13,13 @@ echo "👉 Building app..."
 pnpm build
 
 echo "👉 Setting up environment variable..."
-
 TUNNEL_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[] | select(.name == "localstack") | .public_url')
 if [ -z "$TUNNEL_URL" ]; then
   echo "Failed to get localstack tunnel URL"
   exit 1
 fi
 
-sed -i '' "s|AWS_CLOUDFRONT_URL: ''|AWS_CLOUDFRONT_URL: '$TUNNEL_URL'|" ecosystem.config.js
+echo "👉 Restarting server via PM2 with updated environment variable..."
+pm2 restart ecosystem.config.js --env development --update-env --env AWS_CLOUDFRONT_URL=$TUNNEL_URL
 
-echo "👉 Restarting server via PM2..."
-pm2 restart ecosystem.config.js --env development
-
-echo "✅ Done Staging Local Mac Server"
+echo "✅ Completed"
