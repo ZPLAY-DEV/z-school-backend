e58e9a382e90cc196ab2f74e39d8453585d613d1
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-07-07 10:41:33 +0900

    feat: update ecosystem.config.js to add ngrok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,16 @@ module.exports = {
         NODE_ENV: 'development',
       },
     },
+    {
+      name: 'ngrok',
+      script: 'ngrok', // ngrok 명령 실행
+      args: 'start --all --config="/home/chuck/snap/ngrok/280/.config/ngrok/ngrok.yml"',
+      interpreter: 'none', // Node.js 인터프리터 사용 안함
+      autorestart: true,
+      watch: false,
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      out_file: '~/.pm2/logs/ngrok-access.log',
+      error_file: '~/.pm2/logs/ngrok-error.log',
+    },
   ],
 };
