6fd391d98a563745339baa47467e1dde5992589f
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-07-11 21:15:35 +0900

    feat: update cors option to all

--- src/main.ts
+++ src/main.ts
@@ -65,12 +65,8 @@ async function bootstrap() {
     defaultVersion: '1',
   });
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://zschool.com',
-      'https://zschool.kr',
-    ],
-    credentials: true, // 쿠키를 포함한 요청을 허용하려면 true로 설정
+    origin: '*', // 모든 origin 허용
+    credentials: false, // origin이 *일 때는 credentials를 false로 설정해야 함
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // 허용할 HTTP 메서드
   });
   app.use(helmet());
