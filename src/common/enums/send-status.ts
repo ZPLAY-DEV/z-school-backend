5bb45efe12499bced80d7a5292ff2efe3dd3a681
Chuck JS. Oh <jinseokoh@hotmail.com>
2025-09-06 15:09:39 +0900

    feat: udpate send-status enum

--- src/common/enums/send-status.ts
+++ src/common/enums/send-status.ts
@@ -2,7 +2,6 @@ export enum SendStatus {
   INIT = 'INIT', // 미전송
   SENT = 'SENT', // 전송완료
   SCHEDULED = 'SCHEDULED', // 예약전송
-  RESCHEDULED = 'RESCHEDULED', // 재예약전송
   CANCELED = 'CANCELED', // 취소
   FAILED = 'FAILED', // 전송실패
 }
